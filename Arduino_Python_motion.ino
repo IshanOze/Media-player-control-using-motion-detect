ec9c54cbf292cf649d828d91912423c39e20ae43
IshanOze <72873175+IshanOze@users.noreply.github.com>
2021-04-28 22:10:05 +0530

    Update Arduino_Python_motion.ino

--- Arduino_Python_motion.ino
+++ Arduino_Python_motion.ino
@@ -1,15 +1,16 @@
-const int trig = 9;
-const int echo = 10;
+const int trig = 9;   //digital pin of trigger
+const int echo = 10;   //digital pin of echo
 long duration,cm;
 
 
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(9600);   //Starts the serial monitor
   pinMode(trig, OUTPUT);
   pinMode(echo, INPUT);
  
  }
 
+//function to calculate distance
 void calculate_distance(int trig, int echo)
 {
   digitalWrite(trig, LOW);
@@ -18,10 +19,10 @@ void calculate_distance(int trig, int echo)
   delayMicroseconds(10);
   digitalWrite(trig, LOW);
 
-  duration = pulseIn(echo, HIGH);
+  duration = pulseIn(echo, HIGH);   //duration is taken in time so we need to convert it in cm by the following formula
   cm = (duration/2) * 0.0343;
 
-  if(cm > 58)
+  if(cm > 58)   //you can limit any value for the scale but I found 58 more suitable
     cm = 58;
   
 }
@@ -31,7 +32,7 @@ void loop() {
   calculate_distance(trig, echo);
   
   //Play/Pause
-  if(cm > 25 && cm < 58)
+  if(cm > 25 && cm < 58)    //if any obstruction is introduced between the given scale, it prints 'Play/Pause' in the serial monitor
   {
     Serial.println("Play/Pause");
     delay(500);
@@ -41,18 +42,18 @@ void loop() {
  //Lock  
 if (cm>=10 && cm<=20)
 {
-  delay(100); //Hand Hold Time
+  delay(100); // Hold Time
   calculate_distance(trig,echo);
   
-  if (cm>=10 && cm<=20)
+  if (cm>=10 && cm<=20)   //confirms that the obstruction is locked in the given scale
   {
     Serial.println("Locked");
     while(cm<=50)
     {
       calculate_distance(trig,echo);
-      if (cm > 2 && cm < 15) //Hand pushed in 
+      if (cm > 2 && cm < 15) //pushed in 
       {Serial.println ("Volume Increased"); delay (300);}
-      if (cm > 15 && cm < 30) //Hand pulled out
+      if (cm > 15 && cm < 30) //pulled out
       {Serial.println ("Volume Decreased"); delay (300);}
     }
   }
@@ -64,3 +65,6 @@ if (cm>=10 && cm<=20)
 
   delay(250);
  }
+
+
+/* You can always tweak any of the values defined in the code which is suitable for your needs.*/
